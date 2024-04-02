f16c58f9a3fcfa3a2f27f5353e9c390386c2c661
Miles Murray <41023365+Milpay1@users.noreply.github.com>
2024-04-02 15:38:14 -0400

    add fstream and make set up file

---
+++ HardDriveBenchmark1/HardDriveBenchmark1.cpp
@@ -0,0 +1,14 @@
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+using namespace std;
+using namespace std::chrono;
+
+void HardDriveBenchmark1(){
+    fstream file;
+
+    file.open("file.txt", ios::out | ios::app | ios::binary);
+
+    int loop = pow(10, 7);
+}
