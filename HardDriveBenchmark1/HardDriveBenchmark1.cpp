174f12ca3c27c100ea0b37dde340196434e270ce
Miles Murray <41023365+Milpay1@users.noreply.github.com>
2024-04-02 15:46:51 -0400

    add read and write instructions

--- HardDriveBenchmark1/HardDriveBenchmark1.cpp
+++ HardDriveBenchmark1/HardDriveBenchmark1.cpp
@@ -5,10 +5,20 @@
 using namespace std;
 using namespace std::chrono;
 
-void HardDriveBenchmark1(){
+void HardDriveBenchmark1(char * txt){
     fstream file;
 
     file.open("file.txt", ios::out | ios::app | ios::binary);
 
     int loop = pow(10, 7);
+
+    for(int i=0; i < loop; i++){
+        file.write(txt, 100);
+    }
+
+    file.seekg(0);
+
+    for(int i = 0; i< loop; i++){
+        file.read(txt, 100);
+    }
 }
